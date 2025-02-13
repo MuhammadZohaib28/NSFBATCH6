5c5e8f05d1a4ab4d883e77200e699e67274a41f9
Muhammad Zohaib <muhammad.zohaib@miletap.com>
2025-02-13 22:10:11 +0500

    JAVASCRIPT DAY 1

---
+++ JS/day11.js
@@ -0,0 +1,24 @@
+console.log(35723)
+
+let i = null
+console.log(i)
+
+
+
+
+//DATA TYPES
+// 1 STRINGS - ""
+// 2 NUMBER - 91287 123 1231 2313 123 12
+// 3 BOOLEAN
+// 4 BIGINT - 28734328947328947238947328943274892374239847239847329847
+
+
+var userName = "Muhammad Zohaib"
+
+let userName = "Bisma"
+
+
+console.log(userName)
+
+
+
