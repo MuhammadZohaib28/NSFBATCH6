39534297fcdb66b96b48a7c30c10ee017781d773
Muhammad Zohaib <muhammad.zohaib@miletap.com>
2025-02-14 22:05:17 +0500

    OPERATORS  IN JS

---
+++ JS/day12.js
@@ -0,0 +1,69 @@
+//ARTHIMATIC
+
+console.log(11 % 10)
+console.log(11 + 10)
+console.log(11 - 10)
+console.log(11 / 10)
+console.log(11 * 10)
+
+
+
+let numberOne = 5;
+let numbertwo = 10;
+
+console.log(numberOne + numbertwo);
+
+// =
+// +=
+// -=
+// *=
+// <
+// >
+// <=
+// >=
+
+
+let num = "Hello World"
+
+let additionOperator = 10;
+
+console.log(additionOperator, "BEFORE")
+
+additionOperator += 5;
+
+
+console.log(additionOperator, "AFTER")
+
+
+console.log(18 <= 18)
+
+//COMPARISION OPERATORS
+
+// ==
+// ===
+// !
+// !=
+// !==
+
+let pakistan = "10"
+let india = 10
+
+console.log(!pakistan === india)
+
+
+
+//LOGICAL OPERATORS
+// AND - &&
+// OR - ||
+// NOT - !
+
+let num1 = true
+let num2 = false
+
+console.log(num1 && num2, "AND")
+
+
+
+console.log(num1 || num2, "OR")
+
+console.log(!num1)
